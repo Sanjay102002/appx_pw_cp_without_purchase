bf1d257a979ae22a659fab05a56c89d8beecbf5a
Sanjay102002 <165750829+Sanjay102002@users.noreply.github.com>
2025-06-30 22:22:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from decouple import config
 
-api_id = config('11734178')
-api_hash = config('11e86105f12d9121b04afe06adf4d35f')
-bot_token = config('8052463036:AAFwBwCa9Pfmh6WFZRmEr4SlxZtIqJjSZhU')
-auth_users = config('7575753569', cast=lambda v: [int(x) for x in v.strip('[]').split(',')])
+api_id = config('API_ID')  # Environment variable का नाम
+api_hash = config('API_HASH')
+bot_token = config('BOT_TOKEN')
+auth_users = config('AUTH_USERS', cast=lambda v: [int(x) for x in v.strip('[]').split(',')])
