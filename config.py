a0e4b202d4a2ead0e791c9a8797c55b6b5389d4f
Sanjay102002 <165750829+Sanjay102002@users.noreply.github.com>
2025-06-30 22:16:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-api_id = "11734178"
-api_hash = "11e86105f12d9121b04afe06adf4d35f"
-bot_token = ""
-auth_users = [7575753569]
+from decouple import config
+
+api_id = config('11734178')
+api_hash = config('11e86105f12d9121b04afe06adf4d35f')
+bot_token = config('8052463036:AAFwBwCa9Pfmh6WFZRmEr4SlxZtIqJjSZhU')
+auth_users = config('7575753569', cast=lambda v: [int(x) for x in v.strip('[]').split(',')])
