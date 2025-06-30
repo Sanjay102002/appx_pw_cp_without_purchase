ddb2661e32e0ac87349e9275b8e5c5b86172920b
Sanjay102002 <165750829+Sanjay102002@users.noreply.github.com>
2025-06-30 22:31:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from decouple import config
 
-api_id = config('API_ID')  # Environment variable का नाम
+api_id = config('API_ID')
 api_hash = config('API_HASH')
 bot_token = config('BOT_TOKEN')
 auth_users = config('AUTH_USERS', cast=lambda v: [int(x) for x in v.strip('[]').split(',')])
